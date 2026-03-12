82ca3913a911fc72574125e272488df68d07fa85
hal766yep <155877530+hal766yep@users.noreply.github.com>
2026-03-12 21:39:11 +0800

    Update blog config with new URL

--- blog.config.js
+++ blog.config.js
@@ -1,1 +1,52 @@
-LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-zeta-one-73.vercel.app'
+// 注: process.env.XX是Vercel的环境变量
+
+const BLOG = {
+  API_BASE_URL: process.env.API_BASE_URL || 'https://www.notion.so/api/v3',
+  NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5,en:7c1d570661754c8fbc568e00a01fd70e',
+  THEME: process.env.NEXT_PUBLIC_THEME || 'heo',
+  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN',
+  SINCE: process.env.NEXT_PUBLIC_SINCE || 2021,
+
+  PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false,
+  NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 60,
+  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'auto',
+  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6],
+
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'jamie',
+  BIO: process.env.NEXT_PUBLIC_BIO || '嵌入式软件工程师 & 运动控制工程师🔧 | 热爱技术分享',
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-zeta-one-73.vercel.app',
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 嵌入式, 运动控制',
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico',
+
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true,
+
+  ...require('./conf/comment.config'),
+  ...require('./conf/contact.config'),
+  ...require('./conf/post.config'),
+  ...require('./conf/analytics.config'),
+  ...require('./conf/image.config'),
+  ...require('./conf/font.config'),
+  ...require('./conf/right-click-menu'),
+  ...require('./conf/code.config'),
+  ...require('./conf/animation.config'),
+  ...require('./conf/widget.config'),
+  ...require('./conf/ad.config'),
+  ...require('./conf/plugin.config'),
+  ...require('./conf/performance.config'),
+  ...require('./conf/layout-map.config'),
+  ...require('./conf/notion.config'),
+  ...require('./conf/dev.config'),
+
+  CUSTOM_EXTERNAL_JS: [''],
+  CUSTOM_EXTERNAL_CSS: [''],
+
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true,
+  CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true,
+  LAYOUT_SIDEBAR_REVERSE: process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
+
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是jamie, 欢迎来到我的博客🎉',
+
+  UUID_REDIRECT: process.env.NEXT_PUBLIC_UUID_REDIRECT || false
+}
+
+module.exports = BLOG
